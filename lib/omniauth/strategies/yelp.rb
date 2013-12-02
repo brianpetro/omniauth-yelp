135bbe23c3d534cfa28663288fb362c2ec56d2a7
Brian Petro <brian@angularjobs.com>
2013-12-02 12:47:14 -0500

    v2

--- lib/omniauth/strategies/yelp.rb
+++ lib/omniauth/strategies/yelp.rb
@@ -4,14 +4,14 @@ module OmniAuth
   module Strategies
     class Yelp < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'http://api.yelp.com'
+        :site => 'http://api.yelp.com/v2'
       }
-      
+
       uid { raw_info['id'] }
-      
+
       info do
         {
-          :first_name => raw_info['firstName'], 
+          :first_name => raw_info['firstName'],
           :last_name  => raw_info['lastName'],
           :name       => raw_info['name'],
           :email      => (raw_info['contact'] || {})['email'],
@@ -19,7 +19,7 @@ module OmniAuth
           :location   => raw_info['homeCity']
         }
       end
-      
+
       extra do
         { :raw_info => raw_info }
       end
@@ -28,14 +28,14 @@ module OmniAuth
         options[:authorize_params] = client_params.merge(options[:authorize_params])
         super
       end
-      
+
       def auth_hash
         OmniAuth::Utils.deep_merge(super, client_params.merge({
           :grant_type => 'authorization_code'}))
-      end    
-      
+      end
+
       private
-      
+
       def client_params
         {:client_id => options[:client_id], :redirect_uri => callback_url ,:response_type => "code"}
       end
